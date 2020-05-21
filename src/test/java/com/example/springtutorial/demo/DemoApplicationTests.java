b2ab5da9dc70924626271ff6efc898c6a22c4e39
kshadep7 <kshadep7@gmail.com>
2020-05-20 21:09:09 -0700

    Adding files generated from Spring framework

---
+++ src/test/java/com/example/springtutorial/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtutorial.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
